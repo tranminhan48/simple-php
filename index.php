637d10c7093cb0811d99f2599d322c9bb156da8c
Chung <doctor.ifour@gmail.com>
2018-04-15 10:47:55 +0700

    1

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <h1>To-do List</h1>
+    <hr>
+    <div>
+        <button class="btn btn-danger">Thêm</button>
+        <button class="btn btn-danger">Xóa hết</button>
+    </div>
+    <br>
+    <table class="table table-striped">
+        <thead>
+            <th>#</th>
+            <th>Tên</th>
+            <th>Trạng thái</th>
+            <th>Hành động</th>
+        </thead>
+        <tr>
+            <td>1</td>
+            <td>Ăn Sáng</td>
+            <td>Chưa xong</td>
+            <td>
+                <button class="btn btn-danger">Xong</button>
+                <button class="btn btn-danger">Chỉnh sửa</button>
+                <button class="btn btn-danger">Xóa</button>
+            </td>
+        </tr>
+    </table>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
